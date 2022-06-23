f06046a324fb0435b4660b22f00d7f694fbbdd0f
Travis Koski <tmkoski69@gmail.com>
2022-06-23 12:19:59 -0700

    Index properly renders JSON with the tenants field included

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -1,2 +1,12 @@
 class PropertiesController < ApplicationController
+    def index
+        @properties = Property.all
+        render json: @properties.to_json(:include =>:tenants)
+    end
+    def create
+    end
+    def update
+    end
+    def destroy
+    end
 end
