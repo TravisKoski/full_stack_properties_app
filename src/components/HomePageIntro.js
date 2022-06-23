82b82c901ab7a00b38737ce179d41e7cce52046a
Travis Koski <tmkoski69@gmail.com>
2022-06-23 13:46:25 -0700

    Frontend: Added a nav bar, and a decorated introductory home page

---
+++ src/components/HomePageIntro.js
@@ -0,0 +1,7 @@
+const Intro = () => {
+    return(
+        <h1 style = {{color: "white"}}>Welcome to the property management app</h1> 
+
+    )
+};
+export default Intro
---
+++ src/pages/HomePage.js
@@ -0,0 +1,8 @@
+import Intro from "../components/HomePageIntro.js"
+const Home = () => { return (
+<div className = "win-image">
+    <Intro/>
+</div>
+
+)};
+export default Home
