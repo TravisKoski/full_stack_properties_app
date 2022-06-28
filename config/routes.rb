7e41e9d7016071f35f4eb543178b6e3da8b1e45b
Travis Koski <tmkoski69@gmail.com>
2022-06-28 11:24:49 -0700

    backend: added functionality to tenants controller so that a tenant can now rent a single property

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   resources :tenants
   resources :properties
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.httsml
+  put "tenants/:id/properties/:property_id/rent", to: "tenants#rentProperty"
 
   # Defines the root path route ("/")
   # root "articles#index"
