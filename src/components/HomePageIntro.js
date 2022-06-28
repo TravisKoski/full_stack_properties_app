47a2e641fd25d48bf8ecef5933f8dc3160c1ddb1
Travis Koski <tmkoski69@gmail.com>
2022-06-28 15:46:58 -0700

    Added auth0 login to the frontend, TODO: add logout, and user profile viewing

--- src/components/HomePageIntro.js
+++ src/components/HomePageIntro.js
@@ -1,6 +1,10 @@
+import LoginButton from "./LoginButton";
 const Intro = () => {
     return(
+        <>
         <h1 style = {{color: "white"}}>Welcome to the property management app</h1> 
+        <LoginButton/>
+        </>
 
     )
 };
