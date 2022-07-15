d40ead23540080be6ca8606b36cad361507a74ab
Travis Koski <tmkoski69@gmail.com>
2022-07-15 09:26:57 -0700

    Changed the rent property button to make a rental request instead of directly renting the property

--- src/utils/TenantDashboardUtils.js
+++ src/utils/TenantDashboardUtils.js
@@ -7,12 +7,13 @@ const getTenantById = (id, setTenant) => {
         setTenant(tenantData);
     }).catch((error) => {console.log(error)});
 };
+//creates a rental request which gets sent to the inbox of the property beign rented out
 const rentProperty = (tenantId, propertyId) =>{
-    const requestUrl = `http://localhost:3000/tenants/${tenantId}/properties/${propertyId}/rent`;
-    axios.put(requestUrl)
+    const requestUrl = `http://localhost:3000/rental_requests/create`;
+    const rentalRequestParams = {Property_id: propertyId, Tenant_id: tenantId};
+    axios.post(requestUrl, rentalRequestParams)
     .then((response) =>{
-        const rentedProperty = response.data;
-        alert(`Successfully rented ${rentProperty.name}`);
+        alert(`created rental request! Check your inbox for updates.`);
     })
     .catch((error) => {console.log(error)});
 };
